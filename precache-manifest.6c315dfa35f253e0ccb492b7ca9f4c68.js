865ba817f6233ae2aa2699446ecd7b369fdb6fde
Asus <leinersdvsf16@gmail.com>
2020-10-13 11:46:42 -0500

    Updates

---
+++ precache-manifest.6c315dfa35f253e0ccb492b7ca9f4c68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3164b9244b976dfc2de65aa62dd32c6",
+    "url": "/equipo6_ha_ppi2020/index.html"
+  },
+  {
+    "revision": "505ea5ebefe82dd11d0a",
+    "url": "/equipo6_ha_ppi2020/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3132c78a477f977edede",
+    "url": "/equipo6_ha_ppi2020/static/css/main.8e27bea1.chunk.css"
+  },
+  {
+    "revision": "505ea5ebefe82dd11d0a",
+    "url": "/equipo6_ha_ppi2020/static/js/2.4b13cedd.chunk.js"
+  },
+  {
+    "revision": "2f71fbc01fec0c62d6e32185bc021541",
+    "url": "/equipo6_ha_ppi2020/static/js/2.4b13cedd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3132c78a477f977edede",
+    "url": "/equipo6_ha_ppi2020/static/js/main.9a1d0a62.chunk.js"
+  },
+  {
+    "revision": "445a0bc9b3ca494b3df1",
+    "url": "/equipo6_ha_ppi2020/static/js/runtime-main.f40da8a2.js"
+  }
+]);

1e648748d64ed31a8a8febaae04098b6512adba6
Asus <leinersdvsf16@gmail.com>
2020-11-19 20:05:54 -0500

    cambios en el backend con el mentor

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+const conexion = mysql.createConnection({
+    host: 'blquuwyhye6u7xmtf4yd-mysql.services.clever-cloud.com',
+    user:'ust6o8xko4fonhml',
+    password: 'q4QCPSk0FpV5j9XA9aiw',
+    database: 'blquuwyhye6u7xmtf4yd'
+});
 
 app.get('/saludo', function(request, response){
     response.json({saludo: "hola mundo", descripcion: "esta es mi descripcion"});
@@ -8,3 +15,5 @@ app.get('/saludo', function(request, response){
 app.listen(5057, function(){
     console.log('el servidor se encuentra activo');
 });
+
+conexion.connect();
